d7c47008369343849f54c1ebcacd09447b81efca
Alex Bramartha <91941480+alexbramartha1@users.noreply.github.com>
2023-12-02 16:23:28 +0800

    Creating 4 main page structure and bottom navigation bar

--- app/src/main/java/com/example/wanderwise/MainActivity.kt
+++ app/src/main/java/com/example/wanderwise/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wanderwise
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
