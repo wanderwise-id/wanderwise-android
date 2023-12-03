a7173d0a6b98e8c52a313f90472461e3a8037dd8
Alex Bramartha <91941480+alexbramartha1@users.noreply.github.com>
2023-12-03 13:26:59 +0800

    Creating the behavior of bottom navigation bar to changing between activity

--- app/src/main/java/com/example/wanderwise/HomeActivity.kt
+++ app/src/main/java/com/example/wanderwise/HomeActivity.kt
@@ -1,11 +1,38 @@
 package com.example.wanderwise
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.nav_view)
+        bottomNavigationView.selectedItemId = R.id.navigation_home
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    overridePendingTransition(0, 0)
+                }
+                R.id.navigation_rank -> {
+                    startActivity(Intent(this, RankActivity::class.java))
+                    overridePendingTransition(0, 0)
+                }
+                R.id.navigation_post -> {
+                    startActivity(Intent(this, PostActivity::class.java))
+                    overridePendingTransition(0, 0)
+                }
+                R.id.navigation_profile -> {
+                    startActivity(Intent(this, ProfileActivity::class.java))
+                    overridePendingTransition(0, 0)
+                }
+            }
+            true
+        }
     }
 }
