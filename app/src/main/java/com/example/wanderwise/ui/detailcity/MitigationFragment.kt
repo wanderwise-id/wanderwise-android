1e61a259a63802ac1153e2e6030a937ab6ff10fc
wiratana <andikawiratana@gmail.com>
2023-12-18 19:01:04 +0800

    integrate mitigation section with firebase

--- app/src/main/java/com/example/wanderwise/ui/detailcity/MitigationFragment.kt
+++ app/src/main/java/com/example/wanderwise/ui/detailcity/MitigationFragment.kt
@@ -1,60 +1,69 @@
 package com.example.wanderwise.ui.detailcity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wanderwise.R
+import com.example.wanderwise.data.database.City
+import com.example.wanderwise.data.database.Mitigation
+import com.example.wanderwise.data.database.Score
+import com.example.wanderwise.databinding.FragmentInformationBinding
+import com.example.wanderwise.databinding.FragmentMitigationBinding
+import com.example.wanderwise.ui.adapter.DestinationAdapter
+import com.example.wanderwise.ui.home.HomeViewModel
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.getValue
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [MitigationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MitigationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentMitigationBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var homeViewModel: HomeViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mitigation, container, false)
-    }
+        _binding = FragmentMitigationBinding.inflate(inflater, container, false)
+        val view = binding.root
+        homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
+        val cityKey = homeViewModel.locationData
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MitigationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MitigationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        val db = FirebaseDatabase.getInstance("https://wanderwise-application-default-rtdb.asia-southeast1.firebasedatabase.app")
+
+        val refMitigations = db.getReference("mitigations/${cityKey}")
+
+        val mitigationListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                if(dataSnapshot.childrenCount > 0){
+                    binding.mitigationInfo.text = dataSnapshot.getValue<Mitigation>()!!.content.toString()
+                    binding.mitigationHead.text = dataSnapshot.getValue<Mitigation>()!!.head.toString()
+                } else{
+                    binding.mitigationInfo.text = "-"
+                    binding.mitigationHead.text = "-"
                 }
             }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                Log.w("TAG", "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        refMitigations.addValueEventListener(mitigationListener)
+
+        // Inflate the layout for this fragment
+        return view
     }
 }
